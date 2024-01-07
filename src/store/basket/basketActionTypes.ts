865a839224a31837fe23ca20947c8d2333a5a797
Gabriel Alves <gabe.alvss@gmail.com>
2024-01-07 20:52:07 -0300

    refactor: :label: added basket action types to better readability

---
+++ src/store/basket/basketActionTypes.ts
@@ -0,0 +1,6 @@
+export enum BasketActionTypes {
+  FINISH_REQUEST = "finish_request",
+  CALCULATING_BASKET = "calculating_basket",
+  UPDATE_BASKET = "update-basket",
+  UPDATE_BASKET_TOTALS = "update-basket-totals"
+}
