22aa65b7746ef7658498f2706b86f41fc895ce80
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2024-01-14 19:15:29 -0300

    feature: fix concurrency for changing cart items

--- src/components/basketItem/BasketItem.tsx
+++ src/components/basketItem/BasketItem.tsx
@@ -1,43 +1,51 @@
-
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 
 import BasketItemModel from '../../models/BasketItem';
-import { incrementItem, decrementItem } from '../../store/basket/basketActions';
+import { updateBasket } from '../../store/basket/basketActions';
 
 import './basketItem.scss';
 
 interface BasketItemProps {
-  basketItem: BasketItemModel
+  basketItem: BasketItemModel;
 }
 
 function BasketItem({ basketItem }: BasketItemProps) {
   const dispatch = useDispatch();
 
   const onMinusButtonClick = useCallback(() => {
-    dispatch(decrementItem(basketItem))
-  }, []);
+    const { id, quantity } = basketItem;
+    if (quantity <= 1) return;
+    dispatch(updateBasket(id, quantity - 1));
+  }, [basketItem, dispatch]);
 
   const onPlusButtonClick = useCallback(() => {
-    dispatch(incrementItem(basketItem))
-
-  }, []);
+    const { id, quantity } = basketItem;
+    dispatch(updateBasket(id, quantity + 1));
+  }, [basketItem, dispatch]);
 
   return (
-    <div className="basket-item">
-      <p className='basket-item--info basket-item__name'>Item Name <span>{ basketItem.name }</span></p>
+    <div className='basket-item'>
+      <p className='basket-item--info basket-item__name'>
+        Item Name <span>{basketItem.name}</span>
+      </p>
       <div className='basket-item--info basket-item__quantity'>
         Quantity
-
-        <div className="basket-item__actions">
-          <button className="basket-item__button" onClick={onMinusButtonClick}>-</button>
-          <span>{ basketItem.quantity }</span>
-          <button className="basket-item__button" onClick={onPlusButtonClick}>+</button>
+        <div className='basket-item__actions'>
+          <button className='basket-item__button' onClick={onMinusButtonClick}>
+            -
+          </button>
+          <span>{basketItem.quantity}</span>
+          <button className='basket-item__button' onClick={onPlusButtonClick}>
+            +
+          </button>
         </div>
       </div>
-      <p className='basket-item--info basket-item__total'>Total <span> { basketItem.itemPrice * basketItem.quantity }</span></p>
+      <p className='basket-item--info basket-item__total'>
+        Total <span> {basketItem.itemPrice * basketItem.quantity}</span>
+      </p>
     </div>
-  )
+  );
 }
 
 export default BasketItem;
